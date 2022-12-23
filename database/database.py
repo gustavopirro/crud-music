9abef7ad1cf4f71f7e895437995716652ed5e613
gustavo.pirro <gustavo.pirro@gmail.com>
2022-12-23 19:41:33 -0300

    feat - Created database simulation

---
+++ database/database.py
@@ -0,0 +1,13 @@
+from model import Music
+
+class Database():
+    music_list: list = []
+
+    def add(self, music: Music) -> object:
+        pass
+
+    def retrieve(self, music_name: str) -> object:
+        pass
+
+    def all(self) -> list:
+        pass
