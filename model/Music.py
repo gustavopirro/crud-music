7ea877c7a52e3fa0dc75099563b6cc0ef8d3a672
gustavo.pirro <gustavo.pirro@gmail.com>
2022-12-23 19:17:42 -0300

    config - Initial project architecture
    Created project folders, base functions and classes

---
+++ model/Music.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+class Music():
+    name: str = None
+    artist: str = None
+    published_at: date = None
+
+    def create_object():
+        pass
+
+    def get_all():
+        pass
+
+    def delete_object():
+        pass
+
+    def get_object():
+        pass
