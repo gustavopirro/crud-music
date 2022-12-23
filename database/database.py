2ab843598dc37cb58d4a5b8ab70c4313a298d6fa
gustavo.pirro <gustavo.pirro@gmail.com>
2022-12-23 20:41:35 -0300

    feat - created crud db methods and added db tests

--- database/database.py
+++ database/database.py
@@ -1,13 +1,19 @@
 from model import Music
 
 class Database():
-    music_list: list = []
+
+    def __init__(self) -> None:
+        self.music_list: list = []
 
     def add(self, music: Music) -> object:
-        pass
+        self.music_list.append(music)
+        return music
 
     def retrieve(self, music_name: str) -> object:
-        pass
+        for music in self.music_list:
+            if music.get('name') == music_name:
+                return {'status': 200, 'data': music }
+        return {'status': 404, 'data': None }
 
     def all(self) -> list:
-        pass
+        return self.music_list
---
+++ tests/test_database.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from database.database import Database
+from model.Music import Music
+from datetime import datetime
+
+
+class DatabaseTestCase(TestCase):
+
+    def setUp(self):
+        self.test_database = Database()
+        
+        self.music_mock = { 
+            'name': 'Test Music',
+            'artist': 'Test Artist',
+            'published_at': datetime.now()
+        }
+
+    def test_create_new_db_registry(self):
+        new_music = self.test_database.add(self.music_mock)
+        self.assertEqual(len(self.test_database.music_list) == 1)
+        self.assertEqual(new_music.get('name') == 'Test Music')
+    
+    def test_retrieve_registry_from_db(self):
+        self.test_database.add(self.music_mock)
+        music_obj = self.test_database.retrieve(self.music_mock['name'])
+        
+        self.assertEqual(music_obj['status'], 200)
+        self.assertEqual(music_obj.get('data').get('artist'), 'Test Artist')
+
+    def test_retrieve_invalid_registry_from_db(self):
+        music_obj = self.test_database.retrieve('non existent test music name')
+        
+        self.assertEqual(music_obj['status'], 404)
+    
+    def test_get_music_db_list(self):
+        new_database = Database()
+        new_database.add(self.music_mock)
+        self.assertEqual(len(new_database.all()), 1)
+        new_database.add(self.music_mock)
+        self.assertEqual(len(new_database.all()), 2)
